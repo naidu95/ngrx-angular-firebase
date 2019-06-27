2c85f48224fc982ce850db54fa038630ac3c198a
naidu95 <naidu.kvana@gmail.com>
2019-06-27 18:11:53 +0530

    chnages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import { Observable }   from 'rxjs';
 
 import { User }         from './models/user.models';
 import * as userActions from './actions/user.actions';
+import { AngularFireAuth }            from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 interface AppState {
   user: User;
@@ -18,12 +20,18 @@ interface AppState {
 export class AppComponent {
   user$: Observable<User>;
 
-  constructor(private store: Store<AppState>) {}
+  constructor(private store: Store<AppState>,public auth:AngularFireAuth) {}
 
   ngOnInit() {
-    this.user$ = this.store.select('user');
-console.log( this.user$);
+    this.user$ = this.store.select('user').source;
+    console.log( this.user$);
     this.store.dispatch(new userActions.GetUser());
+    // firebase.auth().onAuthStateChanged(function(user){
+    //   if (user) {
+    //     console.log(user.uid)
+    //   } else {
+    //   }
+    // })
   }
 
   googleLogin() {
@@ -32,6 +40,9 @@ console.log( this.user$);
 
   logout() {
     this.store.dispatch(new userActions.Logout());
+    // if(firebase.auth().currentUser){
+    //   this.auth.auth.signOut();
+    // }
   }
 
 
--- src/app/effects/user.effects.ts
+++ src/app/effects/user.effects.ts
@@ -1,11 +1,11 @@
-import { Injectable }                 from '@angular/core';
-import { Effect, Actions,ofType}            from '@ngrx/effects';
-import { User }                       from '../models/user.models';
+import { Injectable } from '@angular/core';
+import { Effect, Actions,ofType } from '@ngrx/effects';
+import { User } from '../models/user.models';
 
-import { AngularFireAuth }            from 'angularfire2/auth';
-import * as firebase                  from 'firebase';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase';
 
-import { Observable }                 from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/fromPromise';
 import 'rxjs/add/observable/of';
 
@@ -13,23 +13,23 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/delay';
-import 'rxjs/add/operator/filter';
+
 
 import * as userActions from '../actions/user.actions';
 export type Action = userActions.All;
 
+
 @Injectable()
 export class UserEffects {
 
-    constructor(private actions: Actions, private afAuth: AngularFireAuth) {}
-
+    constructor(private actions: Actions, private afAuth: AngularFireAuth) { }
     @Effect()
-    getUser:  Observable<Action> =this.actions.filter(action => action.type === userActions.GET_USER)
- 
-        .map((action: userActions.GetUser) => action.payload )
-        .switchMap(payload => this.afAuth.authState )
+    getUser: Observable<Action> = this.actions.ofType(userActions.GET_USER)
+
+        .map((action: userActions.GetUser) => action.payload)
+        .switchMap(payload => this.afAuth.authState)
         .delay(2000) // delay to show loading spinner, delete me!
-        .map( authData => {
+        .map(authData => {
             if (authData) {
                 /// User logged in
                 const user = new User(authData.uid, authData.displayName);
@@ -38,38 +38,37 @@ export class UserEffects {
                 /// User not logged in
                 return new userActions.NotAuthenticated();
             }
- 
-        })
-        .catch(err =>  Observable.of(new userActions.AuthError()) );
-        
-        login:  Observable<Action> =this.actions.filter(action => action.type === userActions.GOOGLE_LOGIN)
-            .map((action: userActions.GoogleLogin) => action.payload)
-            .switchMap(payload => {
-                return Observable.fromPromise( this.googleLogin() );
-            })
-            .map( credential => {
-                // successful login
-                return new userActions.GetUser();
-            })
-            .catch(err => {
-                return Observable.of(new userActions.AuthError({error: err.message}));
-            });
-     
-     
-        private googleLogin(): Promise<any> {
-            const provider = new firebase.auth.GoogleAuthProvider();
-            return this.afAuth.auth.signInWithPopup(provider);
-        }
 
-        logout:  Observable<Action> =this.actions.filter(action => action.type === userActions.LOGOUT)
-        
-            .map((action: userActions.Logout) => action.payload )
-            .switchMap(payload => {
-                return Observable.of( this.afAuth.auth.signOut() );
-            })
-            .map( authData => {
-                return new userActions.NotAuthenticated();
-            })
-            .catch(err => Observable.of(new userActions.AuthError({error: err.message})) );
-    
+        })
+        .catch(err => Observable.of(new userActions.AuthError()));
+
+    login: Observable<Action> = this.actions.ofType(userActions.GOOGLE_LOGIN)
+
+        .map((action: userActions.GoogleLogin) => action.payload)
+        .switchMap(payload => {
+            return Observable.fromPromise(this.googleLogin());
+        })
+        .map(credential => {
+            // successful login
+            return new userActions.GetUser();
+        })
+        .catch(err => {
+            return Observable.of(new userActions.AuthError({ error: err.message }));
+        });
+
+
+    private googleLogin(): Promise<any> {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        return this.afAuth.auth.signInWithPopup(provider);
+    }
+    logout: Observable<Action> = this.actions.ofType(userActions.LOGOUT)
+
+        .map((action: userActions.Logout) => action.payload)
+        .switchMap(payload => {
+            return Observable.of(this.afAuth.auth.signOut());
+        })
+        .map(authData => {
+            return new userActions.NotAuthenticated();
+        })
+        .catch(err => Observable.of(new userActions.AuthError({ error: err.message })));
 }
